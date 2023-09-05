0ddd3d63df23c71d2b70b2dfc53b1d2ee9065ef9
Daniil Ohorodnik <63656944+SnieFox@users.noreply.github.com>
2023-09-05 16:11:05 +0300

    Работает хехе

--- src/SushiList/SushiList.jsx
+++ src/SushiList/SushiList.jsx
@@ -2,12 +2,16 @@ import './SushiList.scss'
 import { SushiCard } from '../SushiCard/SushiCard';
 import '../../node_modules/bootstrap/scss/bootstrap.scss'
 
-export const SushiList = ({ sushis }) => (
-  <div className='row g-4 justify-content-center'>
-    {
-      sushis.map(sushi => (
-        <SushiCard sushi={sushi} key={sushi.Id} />
-      ))
-    }
-  </div>
-);
+export const SushiList = ({ sushis }) => {
+  console.log(sushis)
+
+  return (
+    <div className='row g-4 justify-content-center'>
+      {
+        sushis.map(sushi => (
+          <SushiCard sushi={sushi} key={sushi.id} />
+        ))
+      }
+    </div>
+  );
+};
